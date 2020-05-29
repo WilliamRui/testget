f8dd43617c7703f03eeb851104045fcbaa2aed9a
william <william@outlook.com>
2020-05-29 11:38:41 +0800

    v01

---
+++ src/test/java/com/william/textget/TextgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.textget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextgetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
